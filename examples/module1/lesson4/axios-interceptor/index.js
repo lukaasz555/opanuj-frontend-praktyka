f3fb319b8371bbb128787de3566354f9e47b8cac
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-06-24 19:30:42 +0200

    axios interceptors

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const endTime = new Date();
+  const diff = endTime - response.config.metadata.startTime;
+  console.log('diff = ', diff);
+
   return response;
 });
 
