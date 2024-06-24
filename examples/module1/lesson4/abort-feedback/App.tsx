171221e05f60c7078aac50fec5028fe41940522b
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-06-24 19:19:59 +0200

    abort feedback

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -9,27 +9,76 @@ const API_URL = '/api/data/users?timeout=10000';
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [isRetryAllowed, setRetryAllowed] = useState(false);
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
+  const fetchData = fetch(API_URL);
+  const timeout = new Promise((_, reject) => {
+    return setTimeout(() => {
+      reject(new Error('Request timeout'));
+    }, 5000);
+  });
+
+  async function fetchUsers() {
+    Promise.allSettled([fetchData, timeout])
+      .then((responses) => {
+        let fetched = false;
+        responses.forEach((response) => {
+          if (
+            response.status === 'fulfilled' &&
+            response.value instanceof Response
+          ) {
+            setError(false);
+            response.value
+              .json()
+              .then((data) => {
+                setUsers(data.users);
+                fetched = true;
+              })
+              .catch((err) => {
+                console.error('Error parsing JSON:', err);
+                setError(true);
+              });
+          }
+        });
+        if (!fetched) {
+          setError(true);
+          setRetryAllowed(true);
+        }
+      })
+      .catch((err) => {
+        console.error('Error fetching users:', err);
+        setError(true);
+        setRetryAllowed(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
+  }
+  useEffect(() => {
+    fetchUsers();
   }, []);
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
-        </div>
+        {isError && (
+          <div className="flex flex-row items-center">
+            <p className="mr-2">
+              Sorry, there seems to be connectivity issues...
+            </p>
+            {isRetryAllowed && (
+              <button
+                className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+                onClick={fetchUsers}
+              >
+                Try again
+              </button>
+            )}
+          </div>
+        )}
       </div>
       <ul className="space-y-2">
         {users.map((user, index) => (
